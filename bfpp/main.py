849f0eb52e4c850989a659db4cf6dbede03e3ad2
loovjo <jonathan.loov@gmail.com>
2019-09-15 15:37:59 +0200

    Add preprocessor with include

--- bfpp/main.py
+++ bfpp/main.py
@@ -1,9 +1,10 @@
 from sys import argv
+from preproc import preproc_file
 from parse import *
 
 if len(argv) == 2:
     # Read file
-    code = open(argv[1]).read()
+    code = preproc_file(argv[1])
 else:
     print("Please provide a file!")
     exit()
