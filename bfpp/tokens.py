ce934a1a28cf3de5017039c2bd622c39e73006dd
loovjo <jonathan.loov@gmail.com>
2019-09-23 19:55:20 +0200

    Add origin ids together with indexed locations

--- bfpp/tokens.py
+++ bfpp/tokens.py
@@ -4,14 +4,34 @@ from add_n_gen import precomp_xyzk_list
 
 class Context:
     def __init__(self):
-        self.locations_with_idxs = {}
-        self.current_ptr = 0
+        self.lwi_list = [{}]
+        self.org_id = 0
+        self.offset = 0
 
         self.macros = INIT_MACROS.copy()
 
     def __str__(self):
-        return "Context(locs=" + str(
-            self.locations_with_idxs) + ", ptr=" + str(self.current_ptr) + ")"
+        return "Context(loc=#{}+{}, lwi={})".format(self.org_id, self.offset,
+                                                    self.locations_with_idxs())
+
+    def locations_with_idxs(self):
+        return self.lwi_list[self.org_id]
+
+    def reorigin_empty(self):
+        self.org_id = len(self.lwi_list)
+        self.offset = 0
+        self.lwi_list.append({})
+
+    def reorigin_copy(self):
+        last_lwi = self.lwi_list[self.org_id]
+
+        self.org_id = len(self.lwi_list)
+
+        self.lwi_list.append(
+            {loc: last_lwi[loc] - self.offset
+             for loc in last_lwi.keys()})
+
+        self.offset = 0
 
 
 class BFPPToken(ABC):
@@ -31,9 +51,9 @@ class BFToken(BFPPToken):
 
     def into_bf(self, ctx):
         if self.token == ">":
-            ctx.current_ptr += 1
+            ctx.offset += 1
         elif self.token == "<":
-            ctx.current_ptr -= 1
+            ctx.offset -= 1
 
         return self.token
 
@@ -68,11 +88,24 @@ class BFLoop(BFPPToken):
         self.inner = inner
 
     def into_bf(self, ctx):
-        ptr_before = ctx.current_ptr
+        last_id = ctx.org_id
+        last_offset = ctx.offset
+
+        ctx.reorigin_copy()
+
+        new_id = ctx.org_id
+
         inner_bf = self.inner.into_bf(ctx)
-        if ctx.current_ptr != ptr_before:
-            # Invalidate locations
-            ctx.locations_with_idxs = []
+        if ctx.org_id == new_id and ctx.offset == 0:
+            # The loop is stable, go back to old state
+            ctx.org_id = last_id
+            ctx.offset = last_offset
+        else:
+            # The loop moved, making it unstable. The loop might have depended on some now unstable
+            # memory location, which makes inner_bf invalid. We will remake it
+            ctx.reorigin_empty()
+            inner_bf = self.inner.into_bf(ctx)
+
         return "[" + inner_bf + "]"
 
     def __str__(self):
@@ -124,7 +157,7 @@ class LocDec(BFPPToken):
     def into_bf(self, ctx):
         for i, name in enumerate(self.locations):
             delta_ptr = i - self.active_idx
-            ctx.locations_with_idxs[name] = ctx.current_ptr + delta_ptr
+            ctx.locations_with_idxs()[name] = ctx.offset + delta_ptr
 
         return ""
 
@@ -141,10 +174,10 @@ class LocGoto(BFPPToken):
         return "LocGoto(" + self.location + ")"
 
     def into_bf(self, ctx):
-        if self.location in ctx.locations_with_idxs:
-            mem_idx = ctx.locations_with_idxs[self.location]
-            delta = mem_idx - ctx.current_ptr
-            ctx.current_ptr = mem_idx
+        if self.location in ctx.locations_with_idxs():
+            mem_idx = ctx.locations_with_idxs()[self.location]
+            delta = mem_idx - ctx.offset
+            ctx.offset = mem_idx
             if delta > 0:
                 return ">" * delta
             else:
@@ -201,25 +234,31 @@ class InvokeMacro(BFPPToken):
             raise ValueError("Wrong number of arguments in call to " +
                              self.name + "!")
 
-        fn_ctx = Context()
-        fn_ctx.current_ptr = ctx.current_ptr
-        fn_ctx.macros = ctx.macros
+        old_org = ctx.org_id
+        old_offset = ctx.offset
+        old_lwi = ctx.locations_with_idxs()
+
+        ctx.reorigin_empty()
+        new_id = ctx.org_id
+        ctx.offset = old_offset
 
         # Assign addresses for arguments
         for i in range(len(self.args)):
             var_name = self.args[i]
             arg_name = fn.args.locations[i]
-            fn_ctx.locations_with_idxs[arg_name] = ctx.locations_with_idxs[
-                var_name]
+            ctx.locations_with_idxs()[arg_name] = old_lwi[var_name]
 
         # Go to the active arg in the function
         goto = LocGoto(fn.args.locations[fn.args.active_idx])
-        code = goto.into_bf(fn_ctx)
+        code = goto.into_bf(ctx)
 
         # Invoke function
-        code += fn.content.into_bf(fn_ctx)
+        code += fn.content.into_bf(ctx)
 
-        ctx.current_ptr = fn_ctx.current_ptr
+        if ctx.org_id == new_id:
+            ctx.org_id = old_org
+        else:
+            ctx.reorigin_empty()
 
         return code
 
@@ -243,7 +282,7 @@ for i in range(256):
     if y == z:
         fn_body = TokenList([
             LocGoto("res"),
-            inc_by(k+x),
+            inc_by(k + x),
         ])
     elif y == 0:
         fn_body = TokenList([
@@ -276,3 +315,10 @@ for i in range(256):
     fn = DeclareMacro("dec" + str(dec_n), args, fn_body)
     INIT_MACROS["dec" + str(dec_n)] = fn
 
+if __name__ == "__main__":
+    ctx = Context()
+    dec = LocDec(["a", "b", "c"], 1)
+    goto = LocGoto("c")
+
+    code = TokenList([dec, goto]).into_bf(ctx)
+    print(code, ctx)
