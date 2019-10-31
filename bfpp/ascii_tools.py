6d6d5837ed5c86e211aa80d034dd1ed31908e9a3
loovjo <jonathan.loov@gmail.com>
2019-10-31 22:35:20 +0100

    Add simple plumbing tools for colors

---
+++ bfpp/ascii_tools.py
@@ -0,0 +1,30 @@
+from colorama import Fore, Back, Style, init
+init()
+
+COL_RESET = Style.RESET_ALL
+
+COL_INFO = Fore.BLUE
+COL_MARKER = Fore.GREEN
+COL_ERROR = Fore.RED
+
+def var(name):
+    return "`" + name + "`"
+
+macro = var
+
+def marker(ch):
+    return COL_MARKER + ch + COL_RESET
+
+def note(note):
+    return COL_INFO + note + COL_RESET
+
+def error(error):
+    return COL_ERROR + error + COL_RESET
+
+def file_location(filename, start_line, end_line):
+    text = "in file " + filename
+    if start_line == end_line:
+        text += " at line " + str(start_line)
+    else:
+        text += " at lines " + str(start_line) + ".." + str(end_line)
+    return COL_INFO + text + COL_RESET
