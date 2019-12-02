df81858b41aa9fade84b72ea5289aaf7d62179c2
loovjo <jonathan.loov@gmail.com>
2019-12-02 21:31:10 +0100

    Quick patch for main to use the new system

--- bfpp/main.py
+++ bfpp/main.py
@@ -1,8 +1,8 @@
 from sys import argv
 from parse import parse
-from context import Context
+from context import State
 from postproc import postproc
-from init_macros import INIT_MACROS
+# from init_macros import INIT_MACROS
 
 if len(argv) == 2:
     # Read file
@@ -13,9 +13,10 @@ else:
 
 tokens = parse(argv[1], code)
 
-ctx = Context()
-ctx.macros = INIT_MACROS
+ctx = State()
+# ctx.macros = INIT_MACROS
+
 res = tokens.into_bf(ctx)
 
-if ctx.n_errors == 0:
-    print(postproc(res))
+#if ctx.n_errors == 0:
+print(postproc(res))
