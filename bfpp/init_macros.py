32c0a5061bce5c357b5c53e0473f14fd22568ed5
loovjo <jonathan.loov@gmail.com>
2019-12-04 20:25:41 +0100

    Don't assume tmp == 0 in incN/subN

--- bfpp/init_macros.py
+++ bfpp/init_macros.py
@@ -17,19 +17,26 @@ def inc_by(n):
 # Generate setN
 for i in range(256):
     (x, y, z, k) = precomp_xyzk_list[i]
+    clear_tmp = TokenList(PREGEN_SPAN, [
+        LocGoto(PREGEN_SPAN, "tmp"),
+        BFLoop(PREGEN_SPAN, True, BFToken(PREGEN_SPAN, "-")),
+    ])
 
     if y == z:
         fn_body = TokenList(PREGEN_SPAN, [
+            clear_tmp,
             LocGoto(PREGEN_SPAN, "res"),
             inc_by(k + x),
         ])
     elif y == 0:
         fn_body = TokenList(PREGEN_SPAN, [
+            clear_tmp,
             LocGoto(PREGEN_SPAN, "res"),
             inc_by(k),
         ])
     else:
         fn_body = TokenList(PREGEN_SPAN, [
+            clear_tmp,
             LocGoto(PREGEN_SPAN, "tmp"),
             inc_by(x),
             BFLoop(
